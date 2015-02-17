9f47c9996db127db4617e87f249743e15b3698e4
rekinyz <rekinyz@hotmail.com>
2015-02-17 16:04:20 +0100

    complete the Junit tests for the YahooDataFetcher

--- src/test/java/com/simstock/fetcher/YahooDataFetcherTest.java
+++ src/test/java/com/simstock/fetcher/YahooDataFetcherTest.java
@@ -1,11 +1,45 @@
 package com.simstock.fetcher;
 
+import java.util.Calendar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class YahooDataFetcherTest {
-	
-	
-	
-	public void testFetcher(){
-		
+
+	private static final String URL = YahooDataFetcher.getBasicURL();
+	private static final Calendar fromDate = Calendar.getInstance();
+	private static final Calendar toDate = Calendar.getInstance();
+	YahooDataFetcher yFetcher = new YahooDataFetcher("GOOG");
+
+	@Test
+	public void testStockName() {
+		assertEquals(URL + "s=GOOG", yFetcher.getURL());
 	}
-	
+
+	@Test
+	public void testFromDate() {
+		fromDate.set(2014, 1, 1);
+		yFetcher = new YahooDataFetcher("GOOG", fromDate);
+		assertEquals(URL + "s=GOOG&b=1&a=0&c=2014", yFetcher.getURL());
+	}
+
+	@Test
+	public void testToDate() {
+		fromDate.set(2014, 1, 1);
+		toDate.set(2015, 1, 1);
+		yFetcher = new YahooDataFetcher("AAPL", fromDate, toDate);
+		assertEquals(URL + "s=AAPL&b=1&a=0&c=2014&e=1&d=0&f=2015", yFetcher.getURL());
+	}
+
+	@Test
+	public void testChangeURL() {
+		fromDate.set(2013, 3, 21);
+		toDate.set(2014, 2, 11);
+		yFetcher.setStockSymbol("YHOO");
+		yFetcher.setFromDate(fromDate);
+		yFetcher.setToDate(toDate);
+		assertEquals(URL + "s=YHOO&b=21&a=2&c=2013&e=11&d=1&f=2014", yFetcher.getURL());
+	}
 }
